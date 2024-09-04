c787262d622a0fd0685dd2d83c037de746dcd75e
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-09-04 10:35:58 +0200

    update plugins list

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -21,8 +21,7 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-      
-      'axios',
+      'axios'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
@@ -97,7 +96,10 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify',
+        'Dialog'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
