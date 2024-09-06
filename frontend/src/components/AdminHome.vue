ebbb014e578cfbfac8e8fcf71dcea86a4116be81
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-09-06 15:44:29 +0200

    updated admin page, add users

--- frontend/src/components/AdminHome.vue
+++ frontend/src/components/AdminHome.vue
@@ -1,24 +1,101 @@
 <template>
   <q-layout>
-    <q-page-container>
-      <q-page class="flex flex-center">
-        AdminHome
-      </q-page>
-    </q-page-container>
+    <q-card-actions class="flex flex-center">
+      <q-btn size="md" label="Add new User" color="primary" @click="() => { this.newUserPrompt = !this.newUserPrompt }" />
+    </q-card-actions>
+
+    <q-dialog v-model="newUserPrompt" persistent>
+      <q-card class="q-pl-mx" style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">New User</div>
+        </q-card-section>
+        <q-form class="q-pt-lg">
+          <q-input
+            filled
+            v-model="this.new.email"
+            label="Email"
+            hint="e.g. user@email.com"
+          />
+          <q-option-group
+            :options="optionsRadio"
+            type="radio"
+            v-model="this.new.role"
+          />
+        </q-form>
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn label="Submit" type="submit" color="primary" v-close-popup class="q-ml-sm" @click="addNewUser()"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <admin-user-table :users="this.users"/>
   </q-layout>
 </template>
 
 <script>
+import API from '../API.js'
+import routes from '../router/routeHandler.js'
+import storage from '../utils/userStorage.js'
+import AdminUserTable from './AdminUserTable.vue'
+
 export default {
+  components: { AdminUserTable },
   name: 'AdminHome',
   data () {
-    return { }
+    return {
+      users: [],
+      newUserPrompt: false,
+      new: {
+        role: undefined,
+        email: undefined,
+        password: 'password' // change this once we have sign-up with email
+      },
+      optionsRadio: [
+        { label: 'Physiotherapist', value: 'physiotherapist' },
+        { label: 'Patient', value: 'patient' },
+      ]
+    }
   },
-  methods: { }
+  async created () {
+    this.newUserPrompt = false
+    await this.getUsers()
+  },
+  methods: {
+     async addNewUser () {
+      try {
+        let user = this.new
+        let resp = await API.addUser(user.role, user.email, user.password)
+        console.log('added: ', resp)
+      } catch (e) {
+        if (e.status === 409) {
+          return this.$q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'User registration failed: ' + e.response.data,
+            icon: 'report_problem'
+          })
+        }
+        this.$q.notify({
+          color: 'negative',
+          position: 'top',
+          message: 'User registration failed' + e,
+          icon: 'report_problem'
+        })
+      }
+      await this.getUsers()
+    },
+    async getUsers () {
+      let res = await API.getUsers()
+      this.users = res
+    }
+  }
 }
 </script>
 
 
 <style scoped>
-
+.userList {
+  max-width: 350px;
+  margin: 0 auto;
+}
 </style>
---
+++ frontend/src/components/AdminUserTable.vue
@@ -0,0 +1,51 @@
+<template>
+    <q-markup-table class="q-ma-lg" v-if="users.length">
+        <thead>
+            <tr>
+                <th class="text-left">Email</th>
+                <th class="text-right">Role</th>
+                <th class="text-right">Created</th>
+                <th class="text-right">Last log in</th>
+            </tr>
+        </thead>
+        <tbody v-for="(user, i) in users" :key="i">
+          <tr :data-id="user.id">
+              <td class="text-left">{{user.email}}</td>
+              <td class="text-right">{{user.role}}</td>
+              <td class="text-right">{{user.createdTimestamp}}</td>
+              <td class="text-right">{{user.lastLoginTimestamp}}</td>
+          </tr>
+        </tbody>
+    </q-markup-table>
+    <div v-else>No user found</div>
+</template>
+
+<script>
+import API from '../API.js'
+import routes from '../router/routeHandler.js'
+import storage from '../utils/userStorage.js'
+
+export default {
+  name: 'AdminUserTable',
+  props: { users: Array },
+  data () {
+    return {
+      columns: [],
+      rows: [],
+      userList: []
+    }
+  },
+  async mounted () { },
+  watch: {
+    users(newValue) { this.userList = newValue }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
