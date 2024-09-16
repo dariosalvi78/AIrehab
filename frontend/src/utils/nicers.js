aa170a26a373bb95efa9d501bb80931b198e8303
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-09-16 17:41:06 +0200

    better datetime format

--- frontend/src/utils/nicers.js
+++ frontend/src/utils/nicers.js
@@ -2,7 +2,16 @@ import { date } from 'quasar'
 const { formatDate } = date
 
 export default {
+    /**
+    * @returns Date with format: YYYY-MM-DD
+    */
     formattedDate(date) {
         return formatDate(date, 'YYYY-MM-DD')
-    }
+    },
+    /**
+    * @returns Date with format: MMM-DD HH:MM
+    */
+    formattedDateLastLogin(date) {
+        return formatDate(date, 'DD MMM HH:mm')
+    },
 }
