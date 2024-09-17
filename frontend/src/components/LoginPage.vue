b5501bd5a7f46959f7d2d63951018dc56751bcbf
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-09-17 11:01:27 +0200

    updated login page to be responsive

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -2,17 +2,47 @@
   <q-layout>
     <q-page-container>
       <q-page class="flex flex-center">
-        <q-form ref="loginForm">
-          <q-card class="q-pa-sm">
+        <q-form ref="loginForm" class="loginForm">
+          <q-card flat class="q-pa-sm">
             <q-card-section>
-              <div class="text-h4">Physiotherapist Sign-in</div>
+              <div class="text-h4">AI Rehab Sign-in</div>
             </q-card-section>
             <q-card-section>
               <form autocomplete="on">
-                <q-input v-model.trim="email" type="email" label="Email" placeholder="e.g. email@email.com"
-                  autocomplete="on" />
-                <q-input v-model.trim="password" type="password" label="Password" autocomplete="on"
-                  @keyup.enter="login()"/>
+                <q-input
+                  class="q-my-md"
+                  v-model.trim="email"
+                  type="email"
+                  label="Email"
+                  placeholder="e.g. email@email.com"
+                  autocomplete="on"
+                >
+                  <template v-slot:before>
+                    <q-icon name="person" />
+                  </template>
+                </q-input>
+                <q-input 
+                  class="q-my-md"
+                  v-model.trim="password"
+                  :type="(!showPassword) ? 'password' : 'text'"
+                  label="Password"
+                  autocomplete="on"
+                  @keyup.enter="login()"
+                >
+                  <template v-slot:before>
+                      <q-icon name="lock" />
+                    </template>
+                  <template v-slot:append>
+                  <q-icon
+                    :name="showPassword ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="showPassword = !showPassword"
+                  />
+                </template>
+              </q-input>
+              <div class="flex row justify-end">
+                <q-btn class="q-pr-none" flat size="sm" label="Forgot password" @click="resetPassword()" />
+              </div>
               </form>
             </q-card-section>
             <q-card-actions class="flex flex-center">
@@ -34,7 +64,8 @@ export default {
     return {
         // TODO: add email form validation
         email: undefined,
-        password: undefined
+        password: undefined,
+        showPassword: false
     }
   },
   methods: {
@@ -45,7 +76,6 @@ export default {
         if (data.user) {
           if (data.user.role == 'admin') this.$router.push('admin')
           else if (data.user.role == 'physiotherapist') this.$router.push('physiotherapist')
-          // else if (data.user.role == 'patient') this.$router.push('patient')
         }
       } catch (err) {
         this.$q.notify({
@@ -65,5 +95,8 @@ export default {
 
 
 <style scoped>
-
+.loginForm {
+  width: 80%;
+  max-width: 500px;
+}
 </style>
