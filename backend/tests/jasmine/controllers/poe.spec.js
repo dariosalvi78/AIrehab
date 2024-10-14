06e503d4a5021bb1a000f89812b6a6f04bbf3d2e
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-10-14 10:15:48 +0200

    updated poe.spec with getPOEEvaluation, sendVideoForPOEEvaluation

--- backend/tests/jasmine/controllers/poe.spec.js
+++ backend/tests/jasmine/controllers/poe.spec.js
@@ -5,18 +5,20 @@ import exercises from '../../../src/controllers/exercises.js'
 import config from '../../../src/utils/config.js'
 import poe from '../../../src/controllers/poe.js'
 import mock from '../../mock_data.js'
+import poeMotionAnalysis from '../../../src/utils/poeMotionAnalysis.js'
 
 beforeAll(function () {
     this.physiotherapist = mock.physiotherapist
+    this.patient = mock.patient
     this.exercises = mock.exercises
     this.poe = mock.poe
 })
 
-describe('getEvaluation access:', function () {
+describe('getPOEEvaluation access:', function () {
 
     it('get evaluation requires authentication', async function () {
         spyOn(poeCollection, 'getEvaluationFromID')
-        await poe.getEvaluation({ user: undefined }, {
+        await poe.getPOEEvaluation({ user: undefined }, {
             sendStatus(status) {
                 expect(status).toBe(403)
                 expect(poeCollection.getEvaluationFromID).not.toHaveBeenCalled()
@@ -25,7 +27,7 @@ describe('getEvaluation access:', function () {
     })
     it('get evaluation requires params exercise id', async function () {
         spyOn(poeCollection, 'getEvaluationFromID')
-        await poe.getEvaluation({
+        await poe.getPOEEvaluation({
             user: this.physiotherapist,
             params: {}
         }, {
@@ -38,7 +40,7 @@ describe('getEvaluation access:', function () {
     it('generic error on get poe from exercise', async function () {
         let exercise = this.exercises[0]
         spyOn(poeCollection, 'getEvaluationFromID').and.returnValue(undefined)
-        await poe.getEvaluation({
+        await poe.getPOEEvaluation({
             user: { role: 'admin' },
             params: { exerciseID: exercise.id }
         }, {
@@ -48,10 +50,46 @@ describe('getEvaluation access:', function () {
             }
         })
     })
+    it('try to get poe results but video is processing', async function () {
+        let exercise = this.exercises[0], physiotherapist = this.physiotherapist, patient = this.patient
+        spyOn(poeCollection, 'getEvaluationFromID').and.returnValue(undefined)
+        spyOn(exercisesCollection, 'getOneExerciseByEmail').and.returnValue({ patientID: patient.id , ...exercise})
+        spyOn(poeMotionAnalysis, 'isEvaluationOngoing').and.returnValue(true)
+        await poe.getPOEEvaluation({
+            user: physiotherapist,
+            params: { exerciseID: exercise.id }
+        }, {
+            sendStatus(status) {
+                expect(status).toBe(102)
+                expect(poeCollection.getEvaluationFromID).toHaveBeenCalledWith(exercise.id)
+                expect(exercisesCollection.getOneExerciseByEmail).toHaveBeenCalledWith(exercise.id, physiotherapist.email)
+                expect(poeMotionAnalysis.isEvaluationOngoing).toHaveBeenCalledWith(patient.id)
+            }
+        })
+    })
+    it('get latest poe results after video is done processing', async function () {
+        let exercise = this.exercises[0], physiotherapist = this.physiotherapist, patient = this.patient
+        spyOn(poeCollection, 'getEvaluationFromID').and.returnValue(undefined)
+        spyOn(exercisesCollection, 'getOneExerciseByEmail').and.returnValue({ patientID: patient.id , ...exercise})
+        spyOn(poeMotionAnalysis, 'isEvaluationOngoing').and.returnValue(false)
+        spyOn(poeMotionAnalysis, 'getLatestPOEAnalysis').and.returnValue([this.poe])
+        await poe.getPOEEvaluation({
+            user: physiotherapist,
+            params: { exerciseID: exercise.id }
+        }, {
+            send(data) {
+                expect(data).toBeDefined()
+                expect(poeCollection.getEvaluationFromID).toHaveBeenCalledWith(exercise.id)
+                expect(exercisesCollection.getOneExerciseByEmail).toHaveBeenCalledWith(exercise.id, physiotherapist.email)
+                expect(poeMotionAnalysis.isEvaluationOngoing).toHaveBeenCalledWith(patient.id)
+                expect(poeMotionAnalysis.getLatestPOEAnalysis).toHaveBeenCalled()
+            }
+        })
+    })
     it('admin can get poe from exercise', async function () {
         let exercise = this.exercises[0]
         spyOn(poeCollection, 'getEvaluationFromID').and.returnValue(this.poe)
-        await poe.getEvaluation({
+        await poe.getPOEEvaluation({
             user: { role: 'admin' },
             params: { exerciseID: exercise.id }
         }, {
@@ -65,7 +103,7 @@ describe('getEvaluation access:', function () {
     it('physiotherapist can get poe from exercise', async function () {
         let exercise = this.exercises[0]
         spyOn(poeCollection, 'getEvaluationFromID').and.returnValue(this.poe)
-        await poe.getEvaluation({
+        await poe.getPOEEvaluation({
             user: this.physiotherapist,
             params: { exerciseID: exercise.id }
         }, {
@@ -78,20 +116,21 @@ describe('getEvaluation access:', function () {
     })
 })
 
-describe('sendEvaluation access:', function () {
+describe('sendVideoForPOEEvaluation access:', function () {
 
     it('send evaluation requires authentication', async function () {
-        spyOn(poeCollection, 'getEvaluationFromID')
-        await poe.sendEvaluation({ user: undefined }, {
+        spyOn(poeMotionAnalysis, 'uploadVideo')
+        await poe.sendVideoForPOEEvaluation({ user: undefined }, {
             sendStatus(status) {
                 expect(status).toBe(403)
-                expect(poeCollection.getEvaluationFromID).not.toHaveBeenCalled()
+                expect(poeMotionAnalysis.uploadVideo).not.toHaveBeenCalled()
             }
         })
     })
     it('cant send evaluation of non existing exercise', async function () {
         spyOn(exercisesCollection, 'getExerciseByID').and.returnValue(undefined)
-        await poe.sendEvaluation({
+        spyOn(poeMotionAnalysis, 'uploadVideo')
+        await poe.sendVideoForPOEEvaluation({
             user: this.physiotherapist,
             params: { exerciseID: 0 }
         }, {
@@ -102,13 +141,15 @@ describe('sendEvaluation access:', function () {
             }, 
             send(data) {
                 expect(data).toContain('Exercise does not exist')
+                expect(poeMotionAnalysis.uploadVideo).not.toHaveBeenCalled()
             }
         })
     })
     it('cant send evaluation if no video exist', async function () {
         let exercise = this.exercises[0]
         spyOn(exercisesCollection, 'getExerciseByID').and.returnValue(exercise)
-        await poe.sendEvaluation({
+        spyOn(poeMotionAnalysis, 'uploadVideo')
+        await poe.sendVideoForPOEEvaluation({
             user: this.physiotherapist,
             params: { exerciseID: exercise.id }
         }, {
@@ -119,49 +160,48 @@ describe('sendEvaluation access:', function () {
             }, 
             send(data) {
                 expect(data).toContain('Video does not exist')
+                expect(poeMotionAnalysis.uploadVideo).not.toHaveBeenCalled()
             }
         })
     })
     it('generic error on send evaluation', async function () {
         let exercise = this.exercises[0]
-        spyOn(poeCollection, 'getEvaluationFromID').and.returnValue(exercise)
-        await poe.sendEvaluation({
+        spyOn(poeMotionAnalysis, 'uploadVideo')
+        await poe.sendVideoForPOEEvaluation({
             user: { role: 'admin' },
             params: { exerciseID: exercise.id }
         }, {
             sendStatus(status) {
                 expect(status).toBe(500)
-                expect(poeCollection.getEvaluationFromID).not.toHaveBeenCalled()
+                expect(poeMotionAnalysis.uploadVideo).not.toHaveBeenCalled()
             }
         })
     })
     it('admin can send evaluation', async function () {
-        let exercise = this.exercises[1]
-        spyOn(exercisesCollection, 'getExerciseByID').and.returnValue(exercise)
-        spyOn(poeCollection, 'updateEvaluationResults').and.returnValue(this.poe)
-        await poe.sendEvaluation({
+        let exercise = this.exercises[1], patient = this.patient
+        spyOn(exercisesCollection, 'getExerciseByID').and.returnValue({ patientID: patient.id, ...exercise })
+        spyOn(poeMotionAnalysis, 'uploadVideo').and.returnValue(true)
+        await poe.sendVideoForPOEEvaluation({
             user: { role: 'admin' },
             params: { exerciseID: exercise.id }
         }, {
-            send(data) {
-                expect(data).toBeDefined()
-                expect(data).toBeInstanceOf(Object)
-                expect(poeCollection.updateEvaluationResults).toHaveBeenCalledWith(exercise.id)
+            sendStatus(status) {
+                expect(status).toBe(200)
+                expect(poeMotionAnalysis.uploadVideo).toHaveBeenCalledWith(patient.id, exercise.videoFile, exercise.type)
             }
         })
     })
     it('physiotherapist can send evaluation', async function () {
-        let exercise = this.exercises[1]
-        spyOn(exercisesCollection, 'getExerciseByID').and.returnValue(exercise)
-        spyOn(poeCollection, 'updateEvaluationResults').and.returnValue(this.poe)
-        await poe.sendEvaluation({
+        let exercise = this.exercises[1], patient = this.patient
+        spyOn(exercisesCollection, 'getExerciseByID').and.returnValue({ patientID: patient.id, ...exercise })
+        spyOn(poeMotionAnalysis, 'uploadVideo').and.returnValue(true)
+        await poe.sendVideoForPOEEvaluation({
             user: this.physiotherapist,
             params: { exerciseID: exercise.id }
         }, {
-            send(data) {
-                expect(data).toBeDefined()
-                expect(data).toBeInstanceOf(Object)
-                expect(poeCollection.updateEvaluationResults).toHaveBeenCalledWith(exercise.id)
+            sendStatus(status) {
+                expect(status).toBe(200)
+                expect(poeMotionAnalysis.uploadVideo).toHaveBeenCalledWith(patient.id, exercise.videoFile, exercise.type)
             }
         })
     })
