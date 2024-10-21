1a7b08c25a22fad476753b83aa14688b51e2a5dc
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-10-21 12:12:04 +0200

    updated set cookie policy on login

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -35,7 +35,7 @@ export default {
 
                 logger.debug({ data: user }, 'user logged in')
                 const token = await signAccessToken(user)
-                res.cookie('token', token)
+                res.cookie('token', token, { sameSite: 'lax', secure: true })
                 return res.send({ user })
             } else {
                 res.sendStatus(404)
