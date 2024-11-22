0a8ba9307a7ba8409c61fcbc441abae80cf26828
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2024-11-22 17:02:17 +0100

    password getPwdFeedback should not show undefined

--- frontend/src/utils/passwordValidation.js
+++ frontend/src/utils/passwordValidation.js
@@ -8,24 +8,24 @@ export default {
         if (password) {
             let checkStrength = password_strength(password)
             if (checkStrength.feedback.warning) {
-                for(let pwd in checkStrength.feedback.suggestions) {
+                for (let pwd in checkStrength.feedback.suggestions) {
                     return `
                         ${checkStrength.feedback.warning}
                         ${checkStrength.feedback.suggestions[pwd]}
                         `
                 }
             }
-           
         }
     },
     getPwdFeedback (password) {
-        if (password) {
-            let pwdHint = '', pwd_score = undefined
-            let checkStrength = password_strength(password)
-            pwd_score = checkStrength.score
-            if (pwd_score >= 2 && pwd_score < 4) pwdHint = '· Good password'
-            else if (pwd_score >= 4) pwdHint = '· Very good password!' 
-            return pwdHint
-        }     
+        if (!password) return ''
+
+        let pwdHint = '', pwd_score = undefined
+        let checkStrength = password_strength(password)
+
+        pwd_score = checkStrength.score
+        if (pwd_score >= 2 && pwd_score < 4) pwdHint = '· OK password'
+        else if (pwd_score >= 4) pwdHint = '· Good password'
+        return pwdHint
     }
 }
