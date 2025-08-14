ed4ba6d9a2a091afeb9bae37a270d59f67ae276c
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2025-08-14 15:49:48 +0200

    update about section article

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -37,21 +37,18 @@
         <div class="text-h6">Articles</div>
         <div class="text-body2 text-weight-light q-py-sm">
           • ”Is seeing just believing? Measurement properties of visual assessment of Postural Orientation Errors (POEs) in people with anterior cruciate ligament injury” <a href="https://portal.research.lu.se/portal/files/79603354/Is_seeing_just_believing_e_spik_Jenny_Nae.pdf" target="_blank">https://portal.research.lu.se/portal/files/79603354/Is_seeing_just_believing_e_spik_Jenny_Nae.pdf</a>
-          <br>
-          <br>
+          <br><br>
           • Nae J, Creaby MW, Nilsson G, Crossley K, Ageberg E. Measurement properties of a test battery to assess postural orientation during functional tasks in patients undergoing ACL injury rehabilitation. The Journal of orthopaedic and sports physical therapy. 2017, 47(11):863-873. <a href="https://www.jospt.org/doi/10.2519/jospt.2017.7270" target="_blank">https://www.jospt.org/doi/10.2519/jospt.2017.7270</a>
-          <br>
-          <br>
+          <br><br>
           • Nae J, Creaby MW, Ageberg E. Extended Version of a Test Battery for Visual Assessment of Postural Orientation Errors: Face Validity, Internal Consistency, and Reliability. Phys Ther 2020;100(9):1542-56. doi: 10.1093/ptj/pzaa092. <a href="https://doi.org/10.1093/ptj/pzaa092" target="_blank">https://doi.org/10.1093/ptj/pzaa092</a>
-          <br>
-          <br>
+          <br><br>
           • Nae J, Creaby M, Cronström A, Ageberg E. Sex differences in postural orientation errors and association with objective and patient-reported function in patients with ACL injury: an exploratory cross-sectional study. BMJ Open Sport & Exercise Medicine 2021;7(2):e001045. doi: 10.1136/bmjsem-2021-001045. <a href="https://bmjopensem.bmj.com/content/7/2/e001045" target="_blank">https://bmjopensem.bmj.com/content/7/2/e001045</a>
-          <br>
-          <br>
+          <br><br>
           • Augustsson S, Nae J, Karlsson M, Peterson T, Wollmer P, Ageberg E. Postural orientation, what to expect in youth athletes? A cohort study on data from the Malmö Youth Sport Study. BMC Sports Sci Med Rehabil 2021, 13 (1): 76. DOI: 10.1186/s13102-021-00307-y
-          <br>
-          <br>
+          <br><br>
           • Nae J, Cronström A. Association between sensorimotor function and visual assessment of postural orientation in patients with ACL injury. Physical therapy in sport 2022, 55: 160-167. DOI: <a href="https://doi.org/10.1016/j.ptsp.2022.04.006" target="_blank">https://doi.org/10.1016/j.ptsp.2022.04.006</a> 
+          <br><br>
+          • Nae J, Banega M, Kronström F, Jakobsson A, Creaby MW, Ageberg E. Evaluation of the ability of machine learning-models to assess postural orientation errors during a single-leg squat. JOSPT Methods, 2025, 1 (1): 17-29. <a href="https://www.jospt.org/doi/10.2519/josptmethods.2024.0086 " target="_blank">https://www.jospt.org/doi/10.2519/josptmethods.2024.0086</a>
         </div>
       </q-card-section>
     </q-card>
